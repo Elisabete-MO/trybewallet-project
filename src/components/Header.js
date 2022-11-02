fe73b8ee8a026de07a0c0beb778128d29f4d6e7b
Bete.mrt <bete.mrt@gmail.com>
2022-11-02 02:31:05 -0300

    Segundoo requisito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import '../styles/header.css';
 
 class Header extends Component {
+  state = {
+    total: 0,
+  };
+
   render() {
+    const { total } = this.state;
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header className="box_header">
+        <h1 className="h1_header">TrybeWallet</h1>
+        <h3 className="email_header" data-testid="email-field">{`Email: ${email}`}</h3>
+        <div className="total_curr_div">
+          <h3
+            className="total_header"
+            data-testid="total-field"
+          >
+            {`Despesa Total: ${total}`}
+          </h3>
+          <h3 className="currency_header" data-testid="header-currency-field">BRL</h3>
+        </div>
+      </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string,
+};
+
+Header.defaultProps = {
+  email: 'alguem@alguem.com',
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import { addEmailAction } from '../redux/actions';
+import Header from '../components/Header';
+import '../styles/wallet.css';
 
-class Wallet extends React.Component {
+class Wallet extends Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div className="container_wallet">
+        <Header />
+      </div>
+    );
   }
 }
 
